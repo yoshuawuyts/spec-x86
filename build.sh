e4490fc95db12deb309138817b2f2125dfe77879
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-07-12 00:15:20 +0200

    cleanup

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-yasm -f elf64 -g dwarf2 -l exit.lst exit.asm
-ld -o exit exit.o
-echo created exit.o
+# setup
+file="$(basename "${1%.*}")"
+mkdir -p target/
+
+# compile to linker + object files
+yasm -f elf64 -g dwarf2 -l $file.lst $1
+# compile to binary
+ld -o target/out $file.o
+
+# cleanup
+rm $file.o $file.lst
