cf18d01b5eaa7806b38d4b07a47f6a955e615f2d
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-07-12 00:46:34 +0200

    exit the function

--- src/0x00.asm
+++ src/0x00.asm
@@ -4,9 +4,9 @@ global _start
 _start:
   xor   eax, eax
   lea   rbx, [0]
-  loop  $
-  /* mov   rdx, 0 */
-  /* and   esi, 0 */
-  /* sub   edi, edi */
-  /* push  0 */
-  /* pop   rbp */
+  ; loop  $
+  mov   rdx, 0
+  and   esi, 0
+  sub   edi, edi
+  push  0
+  pop   rbp
